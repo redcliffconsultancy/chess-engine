e45c06c7dd6e41fa7d7f0b7a1259cf21801d9be0
Cloud User <ec2-user@ip-172-31-33-66.eu-west-1.compute.internal>
2024-02-10 22:21:47 +0000

    experiment with receiver functions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Instructions
 
 Download Go
+
+wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin >> ~/.profile
+source ~/.profile
+go version
--- main.go
+++ main.go
@@ -2,71 +2,21 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
-// 1 bit represents the piece
-// location on the board
-var bitBoard uint64
-
-func setBit(n uint64, pos int) uint64 {
-	// bitwise operators
-	// | OR : Takes two numbers as operands and does OR on every bit of two numbers
-	// << left shift: Takes two numbers
-	n |= (1 << pos)
-	return n
+type Player struct {
+	name          string
+	rightHanded   bool
+	maxServeSpeed int
 }
 
-func clearBit(n uint64, pos int) uint64 {
-	var mask uint64 = ^(1 << pos)
-	n &= mask
-	return n
-}
-
-func moveLeft(n uint64, pos int) uint64 {
-	n |= (1 << pos)
-	return n
-}
-
-func moveRight(n uint64, pos int) uint64 {
-	n |= (1 << pos)
-	return n
-}
-
-func moveForward(n uint64, pos int) uint64 {
-	n |= (8 << pos)
-	return n
-}
-
-func moveBackwards(n uint64, pos int) uint64 {
-	n |= (8 >> pos)
-	return n
+func (p *Player) updateServeSpeed(speed int) {
+	p.maxServeSpeed = speed
 }
 
 func main() {
-	bitBoard := setBit(bitBoard, 32)
-	fmt.Println(strconv.FormatUint(bitBoard, 2))
-
-	bitBoard = moveLeft(bitBoard, 32)
-	fmt.Println(strconv.FormatUint(bitBoard, 2))
-	// bitBoard = setBit(bitBoard, 7)
-	// fmt.Println(strconv.FormatUint(bitBoard, 2))
-	// bitBoard = setBit(bitBoard, 7)
-	// fmt.Println(strconv.FormatUint(bitBoard, 2))
-	// bitBoard = setBit(bitBoard, 7)
-	// fmt.Println(strconv.FormatUint(bitBoard, 2))
-
-	// bitBoard = clearBit(bitBoard, 6)
-	// fmt.Println(strconv.FormatUint(bitBoard, 2))
-}
-
-// all pieces take up one space in the array
-// The king can move in 8 possible directions
-
-type King struct {
-	position     string
-	moves        string
-	validMoves   bool
-	illegalMoves bool
-	capture      bool
+	andrew := Player{name: "Bob", rightHanded: true, maxServeSpeed: 0}
+	fmt.Println(andrew)
+	andrew.updateServeSpeed(100)
+	fmt.Println(andrew)
 }
